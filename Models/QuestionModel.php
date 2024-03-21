5cb340f30f6c5cfc22d311f6f35b7e6353ad8775
Herminet Simon <simon.herminet@gmail.com>
2024-03-21 16:21:26 +0100

    Alimentation de l'index question avec listes // formulaire d'ajout de Q #6

--- Models/QuestionModel.php
+++ Models/QuestionModel.php
@@ -2,10 +2,61 @@
 
 namespace App\Models;
 
+use PDO;
+use Exception;
 use App\Core\DbConnect;
 use App\Entities\Question;
-use Exception;
 
 class QuestionModel extends DbConnect
 {
+
+    // ************************************************ FIND QUESTION PAR ID USER ********************************************** 
+    public function find($id_user)
+    {
+        try {
+            $this->request = $this->connection->prepare(
+                "SELECT * FROM question_memorii 
+            WHERE id_user = :id_user"
+            );
+            $this->request->bindValue(':id_user', $id_user);
+            $this->request->execute();
+
+            // Retourner les résultats de la requête
+            return $this->request->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            // Gérer les erreurs éventuelles
+            echo 'Erreur : ' . $e->getMessage();
+            return [];
+        }
+    }
+
+    // ************************************************ AJOUT QUESTION ********************************************************** 
+
+    public function add(Question $question)
+    {
+        try {
+
+            $this->request = $this->connection->prepare(
+                "INSERT INTO question_memorii(titre_question, question_question, reponse_question, id_user)
+                VALUES(:titre_question, :question_question, :reponse_question, :id_user)"
+            );
+
+            $this->request->bindValue(':titre_question', $question->getTitre_question());
+            $this->request->bindValue(':question_question', $question->getQuestion_question());
+            $this->request->bindValue(':reponse_question', $question->getReponse_question());
+            $this->request->bindValue(':id_user', $question->getId_user());
+
+
+            $this->request->execute();
+        } catch (Exception $e) {
+
+            echo 'Erreur : ' . $e->getMessage();
+        }
+    }
+    // ************************************************ UPDATE QUESTION  ********************************************** 
+
+
+    // ************************************************ DELETE QUESTION  ********************************************** 
+
+
 }
---
+++ Views/question/addQuestion.php
@@ -0,0 +1,32 @@
+<?php
+$title = "J'ajoute une question";
+?>
+
+<div id="formAdd">
+    <div id="addForm">
+        <h1>J'ajoute une nouvelle question : </h1>
+        <form id="envoyer" action="#" method="post">
+            <div class="form-group">
+                <label for="titre">
+                    <h4>titre :</h4>
+                </label>
+                <input class="textQuestion" id="titre" type="text" name="titre_question">
+            </div>
+            <div class="form-group">
+                <label for="question">
+                    <h4>Question :</h4>
+                </label>
+                <textarea id="question" name="question_question"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="reponse">
+                    <h4>Reponse :</h4>
+                </label>
+                <input class="textQuestion" type="text" name="reponse_question">
+            </div>
+            <input type="hidden" value='<?php echo $_SESSION['id_user'] ?>' name="id_user">
+            <button type="submit" name='form_add_question' class="btn-envoyer">
+                <h4>Envoyer</h4>
+            </button>
+        </form>
+    </div>
