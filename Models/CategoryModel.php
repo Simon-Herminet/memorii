9f0c3eec63dc41236c6224a8ab99a27bf26afaa3
Herminet Simon <simon.herminet@gmail.com>
2024-03-22 16:37:06 +0100

    Ajout fonctionnel: reste a prevoir le lien entre Cat et Q #10

--- Models/CategoryModel.php
+++ Models/CategoryModel.php
@@ -5,21 +5,22 @@ namespace App\Models;
 use PDO;
 use Exception;
 use App\Core\DbConnect;
-use App\Entities\Question;
+use App\Entities\Category;
+
 
 class CategoryModel extends DbConnect
 {
 
-
+    // *********************************** Methode pour trouver / compter le nombre de question par catégorie **************************
     public function findCatAndCount($userId)
     {
         try {
             $this->request = $this->connection->prepare("
             SELECT c.id_category, c.titre_category, c.description_category, COUNT(q.id_question) AS number_of_questions
             FROM category_memorii c
-            INNER JOIN question_memorii q ON c.id_category = q.id_category
-            INNER JOIN user_memorii u ON q.id_user = u.id_user
-            WHERE u.id_user = :user_id
+            LEFT JOIN question_memorii q ON c.id_category = q.id_category
+            LEFT JOIN user_memorii u ON q.id_user = u.id_user
+            WHERE (u.id_user = :user_id OR c.id_user = :user_id)
             GROUP BY c.id_category
         ");
             $this->request->bindValue(':user_id', $userId);
@@ -30,6 +31,30 @@ class CategoryModel extends DbConnect
         } catch (Exception $e) {
             echo "Erreur : " . $e->getMessage();
             return false;
+        }
+    }
+
+
+    // ***********************************  METHODE ADD   **************************************************
+    public function add(Category $category)
+    {
+
+        try {
+
+            $this->request = $this->connection->prepare(
+                "INSERT INTO category_memorii(titre_category, description_category, id_user)
+                VALUES(:titre_category, :description_category, :id_user)"
+            );
+
+            $this->request->bindValue(':titre_category', $category->getTitre_category());
+            $this->request->bindValue(':description_category', $category->getDescription_category());
+            $this->request->bindValue(':id_user', $category->getId_user());
+
+
+            $this->request->execute();
+        } catch (Exception $e) {
+
+            echo 'Erreur : ' . $e->getMessage();
         }
     }
 }
---
+++ Views/category/addCategory.php
@@ -0,0 +1,26 @@
+<?php
+$title = "J'ajoute une categorie";
+?>
+
+<div id="formAdd">
+    <div id="addForm">
+        <h1>J'ajoute une nouvelle categorie : </h1>
+        <form id="envoyer" action="#" method="post">
+            <div class="form-group">
+                <label for="titre">
+                    <h4>Titre :</h4>
+                </label>
+                <input class="textQuestion" id="titre" type="text" name="titre_category">
+            </div>
+            <div class="form-group">
+                <label for="question">
+                    <h4>Description :</h4>
+                </label>
+                <textarea id="question" name="description_category"></textarea>
+            </div>
+            <input type="hidden" value='<?php echo $_SESSION['id_user'] ?>' name="id_user">
+            <button type="submit" name='form_add_category' class="btn-envoyer">
+                <h4>Envoyer</h4>
+            </button>
+        </form>
+    </div>
