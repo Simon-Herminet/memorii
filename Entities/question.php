019b56c2c2f7fa28d4abf52756702fc6bf5686e7
Herminet Simon <simon.herminet@gmail.com>
2024-03-25 16:37:08 +0100

    liaison question au catégorie. #37

--- Entities/question.php
+++ Entities/question.php
@@ -152,4 +152,24 @@ class question
 
         return $this;
     }
+
+    /**
+     * Get the value of id_category
+     */
+    public function getId_category()
+    {
+        return $this->id_category;
+    }
+
+    /**
+     * Set the value of id_category
+     *
+     * @return  self
+     */
+    public function setId_category($id_category)
+    {
+        $this->id_category = $id_category;
+
+        return $this;
+    }
 }
